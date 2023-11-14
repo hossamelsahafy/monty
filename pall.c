f67a5a754368a2704f62462ddbe97f31f6e581e2
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-15 00:41:04 +0200

    prints all the values on the stack, starting from the top of the stack

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @stack: parameter that point to linked list
+ *
+ * @l_number: parameter to identify unsigned int 
+*/
+void pall(stack_t **stack, unsigned int l_number)
+{
+    stack_t *c;
+
+    (void)l_number;
+    c = *stack;
+    while (c != NULL)
+    {
+        printf("%d\n", c->n);
+        c = c->next;
+    }
+}
---
+++ push.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * push - function that add element to the top of the stack
+ *
+ * @stack: parameter point to linked list
+ *
+ * @l_number: parameter to identify unsigned int
+ *
+ * @n: parameter to identify int
+*/
+void push(stack_t **stack, unsigned int l_number, int n)
+{
+    stack_t *new;
+
+    (void)l_number;
+    new = malloc(sizeof(stack_t));
+    if (new == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new->n = n;
+    new->prev = NULL;
+    new->next = *stack;
+    if (*stack != NULL)
+    {
+        (*stack)->prev = new;
+    }
+    *stack = new;
+}
