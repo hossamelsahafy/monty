43e5ae95bfbf96d5a2896606cbfcadd89a25991b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-15 19:21:21 +0200

    function that free nodes

--- free_stack.c
+++ free_stack.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 #include <stddef.h>
+/**
+ * f_stack - function to free nodes
+ *
+ * @stack: parameter point to linked list
+ *
+ * @l_number: parameter to identify unsigned int  
+*/
 void free_stack(stack_t **stack, unsigned int l_number)
 {
     while(*stack != NULL)
