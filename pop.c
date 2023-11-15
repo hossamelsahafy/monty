842cda72d100f5c7d69a915c761e5304c895f21b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-15 19:09:57 +0200

    function that delete item

--- pop.c
+++ pop.c
@@ -1,6 +1,13 @@
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * pop - function that delete first item of elements
+ *
+ * @stack: parameter that point to stack_t
+ *
+ * @l_number: parameter to identify unsigned int
+*/
 void pop(stack_t **stack, unsigned int l_number)
 {
     stack_t *tmp;
