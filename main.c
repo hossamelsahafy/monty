6dbd84258ad621babb593a8abf0b329fddeb6e29
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-15 20:31:01 +0200

    Entry function

--- main.c
+++ main.c
@@ -10,30 +10,34 @@
  * @arc: parameter to identify int
  *
  * @arv: parameter point to char
+ *
+ * Return: if arc != 2 return 1
+ * otherwise 0
 */
 int main(int arc, char *arv[])
 {
-    FILE *file;
-    stack_t *stack = NULL;
-    char cmd[MAX_CMD_LEN];
-    unsigned int line_number = 0;
+	FILE *file;
+	stack_t *stack = NULL;
+	char cmd[MAX_CMD_LEN];
+	unsigned int l_number = 0;
 
-    if (arc != 2)
-    {
-        fprintf(stderr, "USAGE: %s filename\n", arv[0]);
-        return 1;
-    }
-    file = fopen(arv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error opening file.\n");
-        return 1;
-    }
-    while (fgets(cmd, MAX_CMD_LEN, file) != NULL)
-    {
-        line_number++;
-        execute_command(cmd, &stack, line_number);
-    }
-    fclose(file);
-    return 0;
+	if (arc != 2)
+	{
+		fprintf(stderr, "USAGE: %s filename\n", arv[0]);
+		return (1);
+	}
+	file = fopen(arv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error opening file.\n");
+		return (1);
+	}
+	while (fgets(cmd, MAX_CMD_LEN, file) != NULL)
+	{
+		line_number++;
+		execute_command(cmd, &stack, l_number);
+	}
+	fclose(file);
+	return (0);
 }
+
