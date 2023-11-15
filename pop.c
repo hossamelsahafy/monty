73d6594350d51959380526f696dfaba0b3eb5aaf
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-15 20:23:04 +0200

    function to delete function

--- pop.c
+++ pop.c
@@ -10,18 +10,19 @@
 */
 void pop(stack_t **stack, unsigned int l_number)
 {
-    stack_t *tmp;
+	stack_t *tmp;
 
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", l_number);
-        exit(EXIT_FAILURE);
-    }
-    tmp = *stack;
-    *stack = (*stack)->next;
-    if (*stack != NULL)
-    {
-        (*stack)->prev = NULL;
-    }
-    free(tmp);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", l_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(tmp);
 }
+
