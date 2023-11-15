d3ed4246e97b63ee85c0c5d1f0c6c00ce49c341c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-15 19:17:41 +0200

    update to header file

--- monty.h
+++ monty.h
@@ -32,7 +32,7 @@ typedef struct instruction_s
 } instruction_t;
 void push(stack_t **stack, unsigned int l_number, int n);
 
-void pall(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int l_number);
 
 void pop(stack_t **stack, unsigned int l_number);
 
