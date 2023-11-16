88707668364dd84672f4cf31b4dc34c2b948762e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-16 16:07:55 +0200

    function that do nothing

---
+++ nop.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * nop - function that do no thing
+ *
+ * @stack: parameter that point to stack_t
+ *
+ * @l_number: parameter that point to unsigned int
+ *
+ * @n: parameter that point to int
+ *
+ * Return: will be 0
+*/
+void nop(stack_t **stack, unsigned int l_number, int n)
+{
+    (void)stack;
+    (void)l_number;
+    (void)n;
+}
