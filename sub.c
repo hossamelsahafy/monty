f9c72b80ae2c6791689f1efa334fd01ae208f11c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-16 18:18:04 +0200

    function to subtract the top element from the second top element

--- sub.c
+++ sub.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * sup - function to add element
+ * sup - function to subtract the top element from the second top element
  *
  * @stack: parameter that point to linked list
  *
@@ -17,7 +17,7 @@ void sub(stack_t **stack, unsigned int l_number, int n)
 	(void)n;
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", l_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", l_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n = (*stack)->next->n - (*stack)->n;
