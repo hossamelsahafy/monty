d0680adfbe4d1f58ff5fecb79b99abb75fc550fa
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-16 15:57:53 +0200

    function to print value of the top element

--- pint.c
+++ pint.c
@@ -9,15 +9,16 @@
  * @l_number: parameter that point to unsigned int
  *
  * @n: parameter that point to int
- * 
+ *
 */
 void pint(stack_t **stack, unsigned int l_number, int n)
 {
-    (void)n;
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", l_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+	(void)n;
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", l_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
+
